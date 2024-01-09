431e4499a54f624e484efacfa46b9225fb320c6f
Evan <evanmschultz@gmail.com>
2024-01-08 23:00:45 -0700

    Adds `ModelType` for type hinting

--- vertix/models/__init__.py
+++ vertix/models/__init__.py
@@ -1,2 +1,5 @@
 from vertix.models.node_model import Node
 from vertix.models.edge_model import Edge
+
+
+ModelType = Node | Edge
--- vertix/typings/__init__.py
+++ vertix/typings/__init__.py
@@ -1,4 +1,5 @@
 import vertix.typings.chroma as chroma
+from vertix.models import ModelType
 
 PrimitiveType = str | int | float | bool
 AttributeDictType = dict[str, str | int | float | bool]
