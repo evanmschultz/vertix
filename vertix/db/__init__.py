668b9bfbaef6293f6fc5cf2529a21d0e08371c8c
Evan <evanmschultz@gmail.com>
2024-01-14 22:42:02 -0700

    Updates db init file for ease of import

--- vertix/db/__init__.py
+++ vertix/db/__init__.py
@@ -1,3 +1,9 @@
 from vertix.db.chroma_db import ChromaDB
 from vertix.db.chroma_client import ChromaDBHandler
 import vertix.db.chroma_setup_client as setup_client
+from vertix.db.chroma_setup_client import (
+    setup_ephemeral_client,
+    setup_persistent_client,
+    setup_http_client,
+)
+from vertix.typings.db import QueryInclude, QueryReturn
