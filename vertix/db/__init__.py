26e3f7d13823638c2e8eb017239f6e28e1e64c1f
Evan <evanmschultz@gmail.com>
2024-01-16 10:31:24 -0700

    Moves chroma_db logic into its own subdirectory

--- vertix/db/__init__.py
+++ vertix/db/__init__.py
@@ -1,7 +1,7 @@
-from vertix.db.chroma_db import ChromaDB
-from vertix.db.chroma_client import ChromaDBHandler
-import vertix.db.chroma_setup_client as setup_client
-from vertix.db.chroma_setup_client import (
+from vertix.db.chroma_db.chroma_db import ChromaDB
+from vertix.db.chroma_db.chroma_client import ChromaDBHandler
+import vertix.db.chroma_db.chroma_setup_client as setup_client
+from vertix.db.chroma_db.chroma_setup_client import (
     setup_ephemeral_client,
     setup_persistent_client,
     setup_http_client,
